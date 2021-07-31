b4d6c7f27c03ffe23608e48daee42336ecef57bb
Mark <markjungersaniva@gmail.com>
2021-07-31 09:13:47 +0800

    Fixed model jsonfield issue

--- app_module/{{cookiecutter.app}}/models/{{cookiecutter.module}}/models.py
+++ app_module/{{cookiecutter.app}}/models/{{cookiecutter.module}}/models.py
@@ -23,6 +23,7 @@ from django_extensions.db import fields as extension_fields
 from django.apps import apps
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
+from django.contrib.postgres.fields import JSONField
 
 from .managers import {{cookiecutter.model}}Manager as manager
 
@@ -42,7 +43,7 @@ class {{cookiecutter.model}}(models.Model):
 
     # === State ===
     is_active = models.BooleanField(default=True)
-    meta = models.JSONField(default=dict, blank=True, null=True)
+    meta = JSONField(default=dict, blank=True, null=True)
 
     # === Relationship Fields ===
     created_by = models.ForeignKey(
